c3fb0cfe4a148f5c1e6bfc2c576cb2b733c6555d
FagnerTimoteo <fagnertimoteosilva@.gmail.com>
2025-03-19 19:31:17 -0300

    env implemented

--- database/conn.js
+++ database/conn.js
@@ -1,16 +1,15 @@
+require("dotenv").config();
 const mongoose = require("mongoose");
 
-async function main(){
+async function main() {
     try {
-        //Retringe que dados não especificados no modelo não sejam enviado para o banco
-        mongoose.set("strictQuery",true);
-        //String de conexão
-        await mongoose.connect('mongodb://127.0.0.1:27017/banco');
-        //Apresenta a mensagem de conexão com a base banco
-        console.log("conectado ao mongodb");
+        mongoose.set("strictQuery", true);
+
+        await mongoose.connect(process.env.MONGO_URI);
+
+        console.log("Conectado ao MongoDB Atlas!");
     } catch (error) {
-        //Captura e exibe alguma exceção que ocorrer
-        console.log(`Error: ${error}`);
+        console.error(`Erro ao conectar ao MongoDB: ${error.message}`);
     }
 }
 
